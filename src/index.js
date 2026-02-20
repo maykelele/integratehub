e423adc8e87395920da5ad71a6c506797ae3dbc1
Sasa Todorovic <sasatodorovic@Sasas-MacBook-Air.local>
2026-02-20 15:36:34 +0100

    delete index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch() {
-    return new Response("IntegrateHub radi 🚀", {
-      headers: {
-        "content-type": "text/plain; charset=utf-8"
-      }
-    });
-  }
-}
